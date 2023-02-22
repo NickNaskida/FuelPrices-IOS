165582adc2a9351e4df6195e91e0c508f88b3709
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2023-02-22 19:25:07 +0400

    Initial Commit

---
+++ FuelPrices/FuelPricesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelPricesApp.swift
+//  FuelPrices
+//
+//  Created by Nikoloz Naskidashvili on 22.02.23.
+//
+
+import SwiftUI
+
+@main
+struct FuelPricesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
